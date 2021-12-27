c46242008f109e8154a05784db7edaac2d680aba
SeungsooChoi <tmdtn5389@gmail.com>
2021-12-27 11:34:26 +0900

    feat : connect apollo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { client } from './apollo';
 import Home from './screens/Home';
 import NotFound from './screens/NotFound';
 import { GlobalStyles } from './styles';
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <GlobalStyles />
       <Router>
         <Routes>
@@ -13,7 +15,7 @@ function App() {
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </Router>
-    </>
+    </ApolloProvider>
   );
 }
 
