ffc1814d944e3eac9cf15dc3445b76955b83ce11
SeungsooChoi <tmdtn5389@gmail.com>
2022-01-09 20:48:56 +0900

    feat : 회원가입 페이지

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import AuthLayout from '../components/user/AuthLayout';
+import Button from '../components/user/Button';
+import Input from '../components/user/Input';
+import Separator from '../components/user/Separator';
+import { mainColor } from '../styles';
+
+const SWrapper = styled.div`
+  margin-top: 10rem;
+  padding: 0rem 20rem;
+`;
+
+const SLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const Logo = styled.h1`
+  font-size: 3rem;
+  font-weight: bold;
+  color: ${mainColor.fontColor};
+`;
 
 const Signup = () => {
-  return <div>회원가입</div>;
+  return (
+    <SWrapper>
+      <AuthLayout>
+        <SLink to="/">
+          <Logo>swim</Logo>
+        </SLink>
+        <form>
+          <Input type="text" name="username" placeholder="username" />
+          <Input type="password" name="password" placeholder="password" />
+          <Button>로그인</Button>
+        </form>
+
+        <Separator />
+      </AuthLayout>
+    </SWrapper>
+  );
 };
 
 export default Signup;
