163fa654e2f9ed391cb4d656fdc41d2b225073d3
SeungsooChoi <tmdtn5389@gmail.com>
2022-01-12 21:52:37 +0900

    feat : httpRequest header에 jwt token 추가

--- src/apollo.js
+++ src/apollo.js
@@ -1,6 +1,28 @@
-import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const TOKEN = 'token';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token,
+    },
+  };
+});
 
 export const isLoggedUser = makeVar(Boolean(localStorage.getItem(TOKEN)));
 
@@ -15,6 +37,6 @@ export const userLogOut = () => {
 };
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
