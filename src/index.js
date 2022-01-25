72233ade4fdb5a3dfa2ed0fee21cb402a00d30a5
SeungsooChoi <tmdtn5389@gmail.com>
2022-01-25 23:28:39 +0900

    feat : tailwind css 설치

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './App';
 import rootReducer from './modules';
+import './index.css';
 
 const store = createStore(rootReducer);
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
 import Map from '../components/Map';
 import NavigationBar from '../components/NavigationBar';
 import PoolList from '../components/PoolList';
 import { elementSize } from '../styles';
 
-const height = window.innerHeight;
-
-const Contents = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  height: ${height - elementSize.navHeight}px;
-`;
+const windowHeight = window.innerHeight;
 
 const Home = () => {
   return (
     <>
       <NavigationBar />
-      <Contents>
+      <div
+        className="flex flex-row justify-between shadow-lg"
+        style={{ height: `${windowHeight - elementSize.navHeight}px` }}
+      >
         <PoolList />
         <Map />
-      </Contents>
+      </div>
     </>
   );
 };
