8c97ce0eb855da12b8125da54b76e354362e7e2e
SeungsooChoi <tmdtn5389@gmail.com>
2022-01-28 12:44:44 +0900

    style: notFound 페이지 디자인수정

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { mainColor } from '../styles';
 
 const NotFound = () => {
   return (
-    <div>
-      <div>존재하지 않는 URL로 접근하셨네요...</div>
-      <div>URL을 확인하시고 다시 시도해주세요</div>
+    <div className="flex flex-col pt-72 items-center w-full h-screen">
+      <FontAwesomeIcon
+        icon={faQuestionCircle}
+        size="5x"
+        color={mainColor.fontColor}
+      />
+      <p className="mt-6 text-center text-lg">
+        <span className="text-2xl">페이지가 존재하지 않습니다.</span>
+        <br />
+        링크를 잘못 입력하셨거나 페이지가 삭제/이동되었을 수 있습니다.
+      </p>
     </div>
   );
 };
