1b5bd3731c9ecb07f722326bfcc8341aceb7f7cb
SeungsooChoi <tmdtn5389@gmail.com>
2022-01-28 20:58:34 +0900

    style : 네비게이션바 프로필 디자인 수정

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -1,32 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 import { userLogOut } from '../../apollo';
 import useUser from '../../hooks/useUser';
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  width: 13rem;
-`;
-const SButton = styled.button`
-  all: unset;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100px;
-  height: 40px;
-  border: 1px solid #dddddd;
-  color: #80c7fa;
-  border-radius: 25px;
-  font-size: 1.2rem;
-  font-weight: 600;
-  cursor: pointer;
-  :hover {
-    color: #1f8cff;
-  }
-`;
 
 const Profile = () => {
   const { data } = useUser();
@@ -36,10 +10,15 @@ const Profile = () => {
   };
 
   return (
-    <Container>
+    <div className="flex flex-row justify-between items-center w-52 text-white font-semibold">
       <span>{data?.seeProfile?.username} 님</span>
-      <SButton onClick={onClick}>로그아웃</SButton>
-    </Container>
+      <button
+        className="px-4 py-3 border border-white rounded-3xl bg-white text-cgBlue hover:bg-slate-100"
+        onClick={onClick}
+      >
+        로그아웃
+      </button>
+    </div>
   );
 };
 
