92d91ada8b15135ec7071bc9e35cfc2e55e631ec
SeungsooChoi <tmdtn5389@gmail.com>
2022-01-28 21:00:49 +0900

    fix : 로그인 한 유저 보여주는 기준 변경

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -11,13 +11,17 @@ const Profile = () => {
 
   return (
     <div className="flex flex-row justify-between items-center w-52 text-white font-semibold">
-      <span>{data?.seeProfile?.username} 님</span>
-      <button
-        className="px-4 py-3 border border-white rounded-3xl bg-white text-cgBlue hover:bg-slate-100"
-        onClick={onClick}
-      >
-        로그아웃
-      </button>
+      {data?.seeProfile?.username && (
+        <>
+          <span>{data.seeProfile.username} 님</span>
+          <button
+            className="px-4 py-3 border border-white rounded-3xl bg-white text-cgBlue hover:bg-slate-100"
+            onClick={onClick}
+          >
+            로그아웃
+          </button>
+        </>
+      )}
     </div>
   );
 };
