068ab6f59ccc36efcd29fb857c566c8d7837da80
SeungsooChoi <tmdtn5389@gmail.com>
2022-01-29 21:00:59 +0900

    feat : react-daum-postcode 설치

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../components/user/Input';
+import DaumPostCode from 'react-daum-postcode';
+import BottomBox from '../components/user/BottomBox';
+import routes from '../routes';
+
+const postCodeStyles = {
+  width: '20rem',
+  height: '30rem',
+};
 
 const Register = () => {
   // Todo
@@ -20,13 +28,23 @@ const Register = () => {
   // 6. 홈페이지
   // 7. 운영시간
   // 8. 기타
+  const [active, setActive] = useState(false);
+
+  const searchAddrClick = e => {
+    e.preventDefault();
+    if (!active) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1>신규 장소 추가</h1>
       <div className="flex flex-row justify-center mt-4 w-full">
         {/* input 요소들이 들어갈 공간 */}
-        <div className="mr-12">
+        <div>
           <form className="mt-10">
             <Input
               text="장소명"
@@ -35,13 +53,23 @@ const Register = () => {
               id="name"
               placeholder="수영장 이름을 입력해주세요."
             />
-            <Input
+            {/* <Input
               text="위치"
               type="text"
               name="position"
               id="position"
               placeholder="임시 항목"
-            />
+            /> */}
+
+            <label className="block mb-4 w-full">
+              <span className="block font-medium text-slate-700">위치</span>
+              <button
+                onClick={searchAddrClick}
+                className="block mt-3 px-3 py-2 w-full border border-slate-400 rounded-md shadow-sm"
+              >
+                &gt;
+              </button>
+            </label>
             <Input
               text="수영장 정보"
               type="text"
@@ -52,8 +80,11 @@ const Register = () => {
           </form>
         </div>
         {/* 장소 위치 추가시 map 객체가 들어갈 공간 */}
-        <div>장소</div>
+        <div className={`${active ? `w-3/12` : `w-0`} overflow-hidden`}>
+          <DaumPostCode />
+        </div>
       </div>
+      <BottomBox cta="" link={routes.home} linkText="처음 화면으로" />
     </div>
   );
 };
