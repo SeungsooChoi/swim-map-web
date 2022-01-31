1333394cf2c9a98d0bdf6aff5b0b82989ade15ac
SeungsooChoi <tmdtn5389@gmail.com>
2022-01-31 14:30:04 +0900

    fix : Input 컴포넌트에 required 추가

--- src/components/user/Input.js
+++ src/components/user/Input.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Input = ({ text, ...rest }) => {
+const Input = ({ text, required, ...rest }) => {
   return (
     <label className="block mb-4 w-full">
-      <span className="block font-medium text-slate-700">{text}</span>
+      <span className="font-medium text-slate-700">{text}</span>
+      {required ? <span className="ml-1 text-red-600">(필수)</span> : ''}
       <input
         className="block mt-3 px-3 py-2 w-full border border-slate-400 rounded-md shadow-sm"
         {...rest}
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Input from '../components/user/Input';
 import BottomBox from '../components/user/BottomBox';
 import routes from '../routes';
+import Button from '../components/user/Button';
 
 const Register = () => {
   // Todo
@@ -34,47 +35,58 @@ const Register = () => {
     }).open();
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('submit');
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1>신규 장소 추가</h1>
-      <div className="flex flex-row justify-center mt-4 w-full">
-        {/* input 요소들이 들어갈 공간 */}
-        <div>
-          <form className="mt-10 w-96">
-            <Input
-              text="장소명"
-              type="text"
-              name="name"
-              id="name"
-              placeholder="수영장 이름을 입력해주세요."
-            />
-            <label className="block mb-4 w-full">
-              <span className="block font-medium text-slate-700">위치</span>
-              <input
-                type="button"
-                name="address"
-                onClick={handleAddressClick}
-                value={address}
-                className="block mt-3 px-3 py-2 w-full border border-slate-400 rounded-md shadow-sm cursor-pointer text-left"
-              />
-            </label>
-            <Input
-              text="상세주소"
-              type="text"
-              name="detailAddress"
-              id="detailAddress"
-              placeholder="상세정보를 입력하세요. (예: 동, 층, 호)"
-            />
-            <Input
-              text="수영장 정보"
-              type="text"
-              name="poolLength"
-              id="poolLength"
-              placeholder="임시 항목"
-            />
-          </form>
-        </div>
-      </div>
+      <h1 className="text-3xl">신규 장소 추가</h1>
+      {/* input 요소들이 들어갈 공간 */}
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center mt-10 mb-4 w-96"
+      >
+        <Input
+          text="장소명"
+          type="text"
+          name="name"
+          id="name"
+          placeholder="수영장 이름을 입력해주세요."
+          required={true}
+        />
+        {/* 위치 주소검색의 경우 공용 Input 컴포넌트 사용이 애매함 */}
+        <label className="block mb-4 w-full relative">
+          <span className="font-medium text-slate-700">위치</span>
+          <span className="ml-1 text-red-600">(필수)</span>
+          <input
+            type="button"
+            name="address"
+            onClick={handleAddressClick}
+            value={address}
+            className="block mt-3 px-3 py-2 w-full border border-slate-400 rounded-md shadow-sm cursor-pointer text-left"
+          />
+          <span className="absolute top-7 right-4 text-2xl text-slate-500 cursor-pointer">
+            &gt;
+          </span>
+        </label>
+        <Input
+          text="상세주소"
+          type="text"
+          name="detailAddress"
+          id="detailAddress"
+          placeholder="상세정보를 입력하세요. (예: 동, 층, 호)"
+        />
+        <Input
+          text="수영장 정보"
+          type="text"
+          name="poolLength"
+          id="poolLength"
+          placeholder="임시 항목"
+        />
+        <Button type="submit">등록</Button>
+      </form>
       <BottomBox cta="" link={routes.home} linkText="처음 화면으로" />
     </div>
   );
