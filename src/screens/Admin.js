56451680df039550ac2e46747735f2fd10369acb
SeungsooChoi <tmdtn5389@gmail.com>
2022-02-02 21:21:54 +0900

    feat : admin 페이지 진행중. 설계 다시 해야할듯

--- src/screens/Admin.js
+++ src/screens/Admin.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { elementSize } from '../styles';
+import AdminNav from './admin/AdminNav';
+
+const windowHeight = window.innerHeight;
+
+const styleHeight = {
+  height: `${windowHeight - elementSize.navHeight}px`,
+};
 
 const Admin = () => {
-  return <div>admin</div>;
+  return (
+    <>
+      <AdminNav />
+      <div
+        className="flex flex-row justify-between shadow-lg"
+        style={styleHeight}
+      >
+        <div className="w-1/4  border-r border-solid border-r-midNightGreen">
+          <div>메뉴</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Admin;
