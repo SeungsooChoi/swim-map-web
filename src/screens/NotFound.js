49112accee61c69b69d23e1097a032a1c34ff382
SeungsooChoi <tmdtn5389@gmail.com>
2022-03-03 22:31:44 +0900

    style(notFound): 스타일 변경

--- src/screens/NotFound.js
+++ src/screens/NotFound.js
@@ -4,24 +4,50 @@ import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 import { mainColor } from '../styles';
 import routes from '../routes';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 18rem;
+  width: 100%;
+  height: 100vh;
+
+  .contents {
+    margin-top: 1.5rem;
+    text-align: center;
+    font-size: 1.125rem;
+    line-height: 1.75rem;
+
+    .note {
+      font-size: 1.5rem;
+      line-height: 2rem;
+    }
+  }
+
+  .link-home {
+    margin-top: 2rem;
+  }
+`;
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col pt-72 items-center w-full h-screen">
+    <Wapper>
       <FontAwesomeIcon
         icon={faQuestionCircle}
         size="5x"
         color={mainColor.fontColor}
       />
-      <p className="mt-6 text-center text-lg">
-        <span className="text-2xl">페이지가 존재하지 않습니다.</span>
+      <p className="contents">
+        <span className="note">페이지가 존재하지 않습니다.</span>
         <br />
         링크를 잘못 입력하셨거나 페이지가 삭제/이동되었을 수 있습니다.
       </p>
-      <Link to={routes.home} className="mt-8">
+      <Link to={routes.home} className="link-home">
         메인 페이지로
       </Link>
-    </div>
+    </Wapper>
   );
 };
 
